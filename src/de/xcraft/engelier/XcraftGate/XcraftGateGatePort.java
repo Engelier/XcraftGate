7551b00956f3102b918ad484e972a9806526300c
Engelier <pennywise@drock.de>
2011-06-08 17:22:32 +0200

    - removed workaround for the crossworld teleportation bug in CraftBukkit 818

--- src/de/xcraft/engelier/XcraftGate/XcraftGateGatePort.java
+++ src/de/xcraft/engelier/XcraftGate/XcraftGateGatePort.java
@@ -15,11 +15,15 @@ public class XcraftGateGatePort implements Runnable {
 	
 	@Override
 	public void run() {
-		if (player.getWorld() != this.location.getWorld()) {
-			// trigger an extra teleport just to change the world
-			// bukkit doesn't implement world- and location-changing in one single step
-			player.teleport(this.location); 
-		}
+/*
+ * 		This got fixed in CB819
+ *  
+ *		if (player.getWorld() != this.location.getWorld()) {
+ *			// trigger an extra teleport just to change the world
+ *			// bukkit doesn't implement world- and location-changing in one single step
+ *			player.teleport(this.location); 
+ *		}
+ */
 		this.player.teleport(this.location);
 	}
 	
