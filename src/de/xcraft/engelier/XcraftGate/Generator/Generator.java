d11bd356b8e42390afd4bc94e02ce15e2a912c3d
Engelier <pennywise@drock.de>
2011-08-09 12:42:02 +0200

    - removed moon generator since this is Dinnerbones source and it has no licence attached to it

--- src/de/xcraft/engelier/XcraftGate/Generator/Generator.java
+++ src/de/xcraft/engelier/XcraftGate/Generator/Generator.java
@@ -9,9 +9,8 @@ import de.xcraft.engelier.XcraftGate.XcraftGate;
 
 public enum Generator {
 	DEFAULT(0),
-	MOON(1),
-	FLATLANDS(2),
-	ISLAND(3);
+	FLATLANDS(1),
+	ISLAND(2);
 	
 	private final int id;
 	private static final Map<Integer, Generator> lookup = new HashMap<Integer, Generator>();
@@ -31,9 +30,8 @@ public enum Generator {
 	public ChunkGenerator getChunkGenerator(XcraftGate plugin) {
 		switch (id) {
 			case 0: return (ChunkGenerator)null;
-			case 1: return new GeneratorMoon();
-			case 2: return new GeneratorFlatlands();
-			case 3: return new GeneratorIsland(plugin);
+			case 1: return new GeneratorFlatlands();
+			case 2: return new GeneratorIsland(plugin);
 		}
 		
 		return null;
